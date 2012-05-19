5981bd9a2401b588ce1ba1910a7c6c0ab62220e0
John Nunemaker <nunemaker@gmail.com>
2012-05-19 16:08:35 -0400

    Move mongo adapter stuff to shared example.

--- spec/mongo_spec.rb
+++ spec/mongo_spec.rb
@@ -3,49 +3,14 @@ require 'helper'
 describe "Mongo adapter" do
   before do
     @client = Mongo::Connection.new.db('test')['test']
-    @adapter = Adapter[:mongo].new(@client)
+    @adapter = Adapter[adapter_name].new(@client)
     @adapter.clear
   end
+
+  let(:adapter_name) { :mongo }
 
   let(:adapter) { @adapter }
   let(:client)  { @client }
 
   it_should_behave_like 'a mongo adapter'
-
-  it "allows using object id's as keys in correct type" do
-    id = BSON::ObjectId.new
-    adapter.write(id, 'ham')
-    client.find_one('_id' => id).should_not be_nil
-    adapter.read(id).should == 'ham'
-  end
-
-  it "stores hashes right in document" do
-    adapter.write('foo', 'steak' => 'bacon')
-    client.find_one('_id' => 'foo').should == {'_id' => 'foo', 'steak' => 'bacon'}
-  end
-
-  describe "with safe option" do
-    before do
-      client.ensure_index([['email', 1]], :unique => true)
-      @adapter = Adapter[:mongo].new(@client, :safe => true)
-    end
-
-    after do
-      client.drop_index('email_1')
-    end
-
-    it "does not raise operation failure on write if operation succeeds" do
-      adapter.write(BSON::ObjectId.new, {'email' => 'john@orderedlist.com'})
-      lambda {
-        adapter.write(BSON::ObjectId.new, {'email' => 'steve@orderedlist.com'})
-      }.should_not raise_error(Mongo::OperationFailure)
-    end
-
-    it "raises operation failure on write if operation fails" do
-      adapter.write(BSON::ObjectId.new, {'email' => 'john@orderedlist.com'})
-      lambda {
-        adapter.write(BSON::ObjectId.new, {'email' => 'john@orderedlist.com'})
-      }.should raise_error(Mongo::OperationFailure)
-    end
-  end
 end
